acaafc9e01f6386a7d91bfc07cbe0da68ea98131
Claudio Hidalgo <wilfridoh721@gmail.com>
2024-06-25 17:38:58 -0500

    Avance Sección 6: Capitulo 27: Formulario Login.

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AuthPageComponent
+  declarations: [    
+    LoginPageComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
